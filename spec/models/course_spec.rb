9df360196e4e9a9e546745ddf9e0af45d1f3119a
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-04-07 09:25:07 -0300

    Add specs to the course and enrollment models

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a professor as a teacher" do
+    teacher = Person.create(is_professor: true)
+    course = Course.create(teacher: teacher)
+    expect(course.teacher.is_professor).to eq true
+  end
 end
--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Enrollment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "Has as many student as it's quota" do
+    teacher = Person.create(is_professor: true)
+    course = Course.create(teacher: teacher, quota: 1)
+    student1 = Person.create
+    Enrollment.create(student: student1, course: course)
+    expect(course.students.count).to eql course.quota
+  end
 end
