86cfa75e5d6c0d006b2b33bba2c1cb3c1dd0b471
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-05-12 08:20:39 -0300

    Add devise to the project

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503103747) do
+ActiveRecord::Schema.define(version: 20170510120423) do
 
   create_table "assignments", force: :cascade do |t|
     t.string   "title"
@@ -54,9 +54,20 @@ ActiveRecord::Schema.define(version: 20170503103747) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.boolean  "is_professor"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_people_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true
   end
 
 end
