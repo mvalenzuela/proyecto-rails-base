12af7da7c6b91501a78e71bf9e6a214d3d41ac67
Ignacio Munizaga <ignacio.munizaga.t@gmail.com>
2017-05-12 08:31:46 -0300

    Revert "Revert "Add devise to the project""
    This reverts commit 5a40bd7858ebca11c446ca3cdee597d80ef9682f.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :enrollments
   has_many :courses, through: :enrollments
 
